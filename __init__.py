a2cc6b153c8bf2ae47c56a6f3e1c4cb37f5c95cf
Stephen Kraemer <s.kraemer@dkfz.de>
2017-07-06 11:26:24 +0200

    Had to comment out conftest and imports
    Made too many changes at once while trying
    to use new method template design pattern.
    
    Have to correct imports and conftest as I reactivate
    the refactored modules

--- __init__.py
+++ __init__.py
@@ -1,35 +1,29 @@
 """
-# TODO: improve type hinting and imports problem
 from . import pileup
-# TODO: PileupSegment only required for type hinting
 from .snp_calling import get_snp_score
 from .meth_pileup import MethPileup
 from .pileup_engines import PysamPileupEngine
-
-# import pyximport
-
-# pyximport.install()
 # from .segments import PileupSegment, PysamPileupSegment
-
 import mqc.methylation_calling
 import mqc.overlap_handling
 import mqc.utilities
 import mqc.conv_err
 """
-from mqc import config
-from mqc import flag_and_index_values
-from mqc import qc_run
-from mqc import counters
-from mqc import index
-from mqc import pileup
-from mqc import mcall
-from mqc.counters.coverage import CoverageCounter
-from mqc.counters.counters_abc import Counter
-from mqc.analysis import run_analysis
-from mqc.pileup.bsseq_pileup_read import BSSeqPileupRead
-from mqc.counters import coverage, mbias
-from mqc.counters.mbias import MbiasCounter
-from mqc.qc_taggers import overlap
-from mqc import qc_taggers
+
+# from mqc import config
+# from mqc import flag_and_index_values
+# from mqc import qc_run
+# from mqc import counters
+# from mqc import index
+# from mqc import pileup
+# from mqc import mcall
+# from mqc.counters.coverage import CoverageCounter
+# from mqc.counters.counters_abc import Counter
+# from mqc.analysis import run_analysis
+# from mqc.pileup.bsseq_pileup_read import BSSeqPileupRead
+# from mqc.counters import coverage, mbias
+# from mqc.counters.mbias import MbiasCounter
+# from mqc.qc_taggers import overlap
+# from mqc import qc_taggers
 
 
