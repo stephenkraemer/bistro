df1d88c7e6d66d5dfa2f618eb56315d46c12fd28
Stephen Kraemer <stephenkraemer@gmail.com>
2018-07-06 15:41:06 +0200

    update read.qc_fail_flag independent of other flags
    - old behavior: qc_fail_flag is only updated if the event
      is not excluded from methylation calling by other flags, e.g.
      overlap_flag or trimm_flag
    - new behavior: phred and mapq fail bits are set in the
      qc_fail_flag for each event, independent of other event
      properties

--- src/mqc/qc_filters.py
+++ src/mqc/qc_filters.py
@@ -1,5 +1,8 @@
 """MotifPileupProcessors for QC filtering"""
 
+# TODO: combine PhredFilter and MapqFilter to avoid cost for double
+# iteration
+
 from mqc.visitors import Visitor
 from mqc.pileup.pileup import MotifPileup
 
@@ -12,36 +15,42 @@ from typing import Dict, Any
 ConfigDict = Dict[str, Any]
 
 class PhredFilter(Visitor):
+    """Tag BSSeqPileupRead with low phred score qcfailflag bit
+
+    Reads are tagged as qcfail with the appropriate flag value if
+    BSSeqPileupRead.baseq_at_pos < min_phred_score.
+
+    Notes:
+    - phred filtering is usually done after overlap handling,
+      because overlap handling can be used to adjust phred scores
+    """
     def __init__(self, config: ConfigDict) -> None:
-        # will keep phred >= min_phred_score
         self.min_phred_score = config['basic_quality_filtering']['min_phred_score']
     def process(self, motif_pileup: MotifPileup) -> None:
+        """Check all reads and flag if appropriate
+
+        The qcfail flag is set independent of the presence of other
+        failure flags: overlap_flag, trimming flag and other qcfail
+        flags are not considered. This is by design. In different
+        scenarios, overlap flags are not considered (mate-stratified
+        methylation calling, M-bias stats), so reads must have
+        phred score fail informations independent of the overlap
+        flag. Similar situations arise for the other fail flags.
+        """
         for curr_read in motif_pileup.reads:
-
-            if (curr_read.qc_fail_flag
-                # phred filtering is usually done after overlap handling,
-                # because overlap handling can be used to adjust phred scores
-                or curr_read.overlap_flag
-                # trimming is done before overlap handling
-                or curr_read.trimm_flag
-                or curr_read.meth_status_flag == mflag.is_na):
-                continue
-
             if curr_read.baseq_at_pos < self.min_phred_score:
                 curr_read.qc_fail_flag |= qflag.phred_score_fail
 
 class MapqFilter(Visitor):
+    """Tag BSSeqPileupRead with mapq fail flag
+
+    Adds mapq_fail flag to BSSeqPileupRead.qflag if
+    BSSeqPileupRead mapping quality is < min_mapq parameter
+    """
     def __init__(self, config: ConfigDict) -> None:
-        # will keep mapq >= min_mapq
         self.min_mapq = config['basic_quality_filtering']['min_mapq']
     def process(self, motif_pileup: MotifPileup) -> None:
         for curr_read in motif_pileup.reads:
-
-            if (curr_read.qc_fail_flag
-                # mapq filter should be applied before overlap calling
-                or curr_read.meth_status_flag == mflag.is_na):
-                continue
-
             if curr_read.alignment.mapping_quality < self.min_mapq:
                 curr_read.qc_fail_flag |= qflag.mapq_fail
 
