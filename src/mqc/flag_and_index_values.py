475dbc40f8dca2ce72de153814314a775348ce90
Stephen Kraemer <stephenkraemer@gmail.com>
2018-07-06 08:06:07 +0200

    fix recently introduced typing.NamedTuple usage for enums

--- src/mqc/flag_and_index_values.py
+++ src/mqc/flag_and_index_values.py
@@ -34,20 +34,25 @@ For example:
     88 ns ± 4.17 ns per loop (mean ± std. dev. of 7 runs, 10000000 loops each)
 """
 
-from collections import namedtuple
+# noinspection PyPep8
 
-bsseq_strand_indices = namedtuple('BsseqIndex', 'c_bc c_bc_rv w_bc w_bc_rv')(
-    c_bc=0,
-    c_bc_rv=1,
-    w_bc=2,
-    w_bc_rv=3,
-)
+from typing import NamedTuple
+
 bsseq_strand_na_index = -1
+
+# The indices for the BS-strands must be kept in sync between strat_call_indices and
+# bsseq_strand_indices, as I have code relying on this
+class BsseqIndex(NamedTuple):
+    c_bc: int = 0
+    c_bc_rv: int = 1
+    w_bc: int = 2
+    w_bc_rv: int = 3
+bsseq_strand_indices = BsseqIndex()
 
 
 # The indices for the BS-strands must be kept in sync between strat_call_indices and
 # bsseq_strand_indices, as I have code relying on this
-class StratCallIndices:
+class StratCallIndices(NamedTuple):
     c_bc: int = 0
     c_bc_rv: int = 1
     w_bc: int = 2
@@ -57,12 +62,14 @@ class StratCallIndices:
     all: int = 6
 strat_call_indices = StratCallIndices()
 
-class MethStatIndices:
+
+class MethStatIndices(NamedTuple):
     n_meth: int = 0
     n_total: int = 1
 meth_status_indices = MethStatIndices()
 
-class MethylationStatusFlags:
+
+class MethylationStatusFlags(NamedTuple):
     is_na: int = 16
     is_methylated: int = 8
     is_unmethylated: int = 4
@@ -70,18 +77,13 @@ class MethylationStatusFlags:
     is_ref: int = 1
 methylation_status_flags = MethylationStatusFlags()
 
-qc_fail_flags = namedtuple('QcFailFlags', 'sam_flag_fail'
-                                          ' phred_score_fail'
-                                          ' mapq_fail'
-                                          ' missing_info_fail'
-                                          ' softclipped'
-                                          ' pos_in_read_exceeds_tlen'
-                                          ' overlap_fail')(
-    sam_flag_fail = 1,
-    phred_score_fail = 2,
-    mapq_fail = 4,
-    missing_info_fail = 8,
-    softclipped = 16,
-    pos_in_read_exceeds_tlen = 32,
-    overlap_fail = 64
-)
+
+class QcFailFlags(NamedTuple):
+    sam_flag_fail: int = 1
+    phred_score_fail: int = 2
+    mapq_fail: int = 4
+    missing_info_fail: int = 8
+    softclipped: int = 16
+    pos_in_read_exceeds_tlen: int = 32
+    overlap_fail: int = 64
+qc_fail_flags = QcFailFlags()
