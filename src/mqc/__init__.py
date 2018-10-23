880faf8a20b6314778a704436264dd717734bfc5
Stephen Kraemer <stephenkraemer@gmail.com>
2018-10-23 10:10:41 +0200

    move bistro snakefiles to separate repo

--- src/mqc/__init__.py
+++ src/mqc/__init__.py
@@ -9,6 +9,3 @@ from mqc.mcall_run import PileupRun
 # from mqc.pileup.pileup import MotifPileup
 
 from pkg_resources import resource_filename
-
-def get_bistro_addons_snakefile():
-    return resource_filename('mqc.resources', 'bistro_workflow_add-ons.smk')
